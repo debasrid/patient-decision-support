1bcde79fe571e35d3acb7dc0a1b8bc8f2934a968
Debasri Dasgupta <46868809+debasrid@users.noreply.github.com>
2021-07-25 22:52:34 +0200

    Heroku config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import patientRouter from './routers/patientRouter.js';
@@ -19,11 +20,16 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://debby:debby123@cluste
 });
 app.use('/api/patients', patientRouter);
 
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Serve at http://localhost:${port}`);
+  console.log(`Server started on port ${port}`);
 });
