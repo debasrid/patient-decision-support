8257617b53d7fea657f111eb3d038944419e07f5
Debasri Dasgupta <46868809+debasrid@users.noreply.github.com>
2021-07-25 23:14:48 +0200

    Heroku config

--- backend/server.js
+++ backend/server.js
@@ -2,34 +2,35 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import config from './config';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import patientRouter from './routers/patientRouter.js';
+import patientRouter from './routers/patientRouter';
 
 dotenv.config();
+const mongodbUrl= config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://debby:debby123@cluster0.tqx5t.mongodb.net/patientdata?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
 app.use('/api/patients', patientRouter);
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, '/../frontend/build')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
+    res.sendFile(path.join(__dirname, '/../frontend/build'))
 })
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = config.PORT;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
